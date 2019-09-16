c02353d7bad9c557114c54503be913edd43796cd
hebisu <hiromitsu.ebisu@gmail.com>
2019-09-16 20:40:03 +0900

    Print comparison process

--- boyer_moore.c
+++ boyer_moore.c
@@ -29,6 +29,7 @@ int init_skiptable(int *skip_table, const char pattern[], const int pattern_len,
 	        if(skip_table[i] < pattern_len)
 	            printf(" char:%c, skip_table[%03d], step:%d\n", (char)i, i, (int)skip_table[i]);
 	    }
+		printf("-----------------------------------\n");
 	}
 
 	return 0;
@@ -42,8 +43,28 @@ int next_position(int *skip_table, const char key, const int nextstep){
 		return nextstep;
 }
 
-void print_comparison_process(const char text[], const char pattern[], const int ptr_text, const int ptr_pattern){
+void print_comparison_process(const char text[], const char pattern[], const int ptr_text, const int ptr_pattern, const int pattern_len){
 // TODO print comparison process function
+    if(!text){
+        fprintf(stderr, "text NULL\n");
+		exit(0);
+    }
+	if(!pattern){
+        fprintf(stderr, "pattern NULL\n");
+		exit(0);
+    }
+
+	for(int i = 0; i < ptr_text; i++)
+		printf(" ");
+	printf("*\n");
+	printf("%s\n", text);
+	for(int i = 0; i < ptr_text - ptr_pattern; i++)
+		printf(" ");
+	printf("%s\n", pattern);
+	for(int i = 0; i < ptr_text; i++)
+		printf(" ");
+	printf("*\n");
+	printf("\n");
 }
 
 int boyermoore_match(const char text[], const char pattern[], const int print_process)
@@ -53,6 +74,7 @@ int boyermoore_match(const char text[], const char pattern[], const int print_pr
 	int skip_table[UCHAR_MAX + 1];		// Skip table
 	int ptr_text = pattern_len - 1;		// Cursor to text (ptr_text starts from the end position of pattern)
 	int ptr_pattern = pattern_len - 1;	// Cursor to pattern (Matching starts from the end character of pattern)
+	int count = 0;
 
 	// Initialize skip table
 	if(init_skiptable(skip_table, pattern, pattern_len, print_process) < 0){
@@ -63,19 +85,25 @@ int boyermoore_match(const char text[], const char pattern[], const int print_pr
 	while (ptr_text < text_len) { // Continue to the end of text
 		ptr_pattern = pattern_len - 1; // Matching starts from the end character of pattern
 
-		if(print_process) print_comparison_process(text, pattern, ptr_text, ptr_pattern);
-
 		while (text[ptr_text] == pattern[ptr_pattern]) {
-			if (ptr_pattern == 0) // Pattern matches a part of text
+			if(print_process) print_comparison_process(text, pattern, ptr_text, ptr_pattern, pattern_len);
+			if (ptr_pattern == 0){ // Pattern matches a part of text
+				printf("Total comparison count:%d\n", count);
 				return ptr_text;
+			}
 			ptr_pattern--;
 			ptr_text--;
+			count++;
 		}
+		count++;
+
+		if(print_process) print_comparison_process(text, pattern, ptr_text, ptr_pattern, pattern_len);
 
 		// Move text pointer to the next position
 		ptr_text += next_position(skip_table, text[ptr_text], pattern_len - ptr_pattern);
 	}
 
+	printf("Total comparison count:%d\n", count);
 	return -1; // Pattern does not match any part of text
 }
 
